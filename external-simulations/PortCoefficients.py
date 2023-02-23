dc9e0fcd6eb33949ecbb48af6454cc8ea62d0b17
michael-petersen <petersen.michael.s@gmail.com>
2023-02-23 16:48:34 +0000

    Fix a few typos and add better usage notes for PortCoefficients

--- external-simulations/PortCoefficients.py
+++ external-simulations/PortCoefficients.py
@@ -1,5 +1,5 @@
 """
-an example script to bring in a Hernquist basis and fill a coefficient file with externally-generated coefficients
+an example script to port an externally-generated Hernquist basis coefficient set and fill an EXP coefficient file 
 (e.g. from gala)
 
 """
@@ -89,11 +89,13 @@ def twopower_density(r,a,alpha,beta):
 
 
 # realise an analytic basis. in this case, it's a hernquist (alpha=1,beta=3)
-hernquist_rs = 15.
-R,D,M,P = makemodel(twopower_density,1.,[hernquist_rs,1,3],rvals = 10.**np.linspace(-1.,3.,2000),pfile='Hernquist_rs{}.dat'.format(hernquist_rs))
+hernquist_rs = 15. # in kpc
+total_mass   = 1.0 # this can be in any units; note that EXP enforces G=1, so the mass will be scaled.
+
+R,D,M,P = makemodel(twopower_density,total_mass,[hernquist_rs,1,3],rvals = 10.**np.linspace(-1.,3.,2000),pfile='Hernquist_rs{}.dat'.format(hernquist_rs))
 
 
-# now, let's generate a basis for GSE. These are the settings, no need to touch these for now.
+# now, let's generate a Hernquist basis inside EXP.
 hernquist_config = """
 id         : sphereSL
 parameters :
@@ -111,9 +113,7 @@ hernquist_basis = pyEXP.basis.Basis.factory(hernquist_config)
 # create a dummy array that you can modify.
 hernquist_coef = hernquist_basis.createFromArray([1.],[[1.],[1.],[1.]], time=0.0)
 print("Generated dummy array with dimensions {}.".format(hernquist_coef.getCoefs().shape))
-# the coefficients are written as imaginary now, so
-# 0: Y_00, 1: Y_10, 2: Y_11, 3: Y_20, 4:Y_21, 5:Y_22, 6:Y_30, 7:Y_31, etc
-# 0:0, 1:1 (1), 3:2 (2), 6:3 (3), 10:4 (4), 15:5 (5), 21:6 (6)
+
 
 # make an instance of coefs
 hernquist_coefs = pyEXP.coefs.Coefs.makecoefs(hernquist_coef, 'halo')
@@ -121,12 +121,18 @@ hernquist_coefs = pyEXP.coefs.Coefs.makecoefs(hernquist_coef, 'halo')
 # here is where you will loop through the times in your simulation. modify the array for each timestep
 
 # change the time each loop through
-hernqusit_coef.time = 0.0
+hernquist_coef.time = 0.0
 
 modcoefs = hernquist_coef.setCoefs()
 # now you can change the array modcoefs and the changes will be reflected in hernquist_coef
 
-# ...plug in your changes
+# ...plug in your changes here
+
+# the coefficients are written as imaginary now, so
+# 0: Y_00, 1: Y_10, 2: Y_11, 3: Y_20, 4:Y_21, 5:Y_22, 6:Y_30, 7:Y_31, etc
+# 0:0, 1:1 (1), 3:2 (2), 6:3 (3), 10:4 (4), 15:5 (5), 21:6 (6)
+
+# following the Lowing et al. (2011) notation, the S coefficients are the real part of EXP coefficients, and the T coefficients are the imaginary part of EXP coefficients.
 
 # add the coefficients to the makecoefs instance
 hernquist_coefs.add(hernquist_coef)
@@ -140,6 +146,8 @@ hernquist_coefs.WriteH5Coefs('outcoef.hernquist')
 # end loop through times
 
 
+# with these coefficients in hand, you can now create density, force, potential, etc.
+
 
 # once done with looping...
 # to bring the file back in,
@@ -148,3 +156,5 @@ hernquist_coefs_in = pyEXP.coefs.Coefs.factory('outcoef.hernquist.h5')
 # now you can navigate the structure as
 # hernquist_coefs_in.Times()
 # hernquist_coefs_in.getAllCoefs()
+
+# you can also use this generated set of coefficients to run mSSA
